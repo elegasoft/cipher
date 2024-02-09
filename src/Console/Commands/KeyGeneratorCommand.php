a197fc159bc303389f81fa113cb0fc9ddd0c95b5
Jason Cook <jason@elegasoft.com>
2024-02-09 08:49:28 -0800

    Enhance key generation and prevent overwriting existing keys
    The code has been updated to allow selective key generation based on key bases. It also adds a check to prevent overwriting of existing keys. These changes improve the flexibility and reliability of the key generation command. The user now has control over which keys to generate and unnecessary overwrites are avoided.

--- src/Console/Commands/KeyGeneratorCommand.php
+++ src/Console/Commands/KeyGeneratorCommand.php
@@ -4,8 +4,10 @@ namespace Elegasoft\Cipher\Console\Commands;
 
 use Elegasoft\Cipher\KeyGenerator;
 use Illuminate\Console\Command;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Storage;
 use Symfony\Component\Console\Command\Command as CommandAlias;
+use function Laravel\Prompts\multiselect;
 use function Laravel\Prompts\text;
 
 class KeyGeneratorCommand extends Command
@@ -33,20 +35,58 @@ class KeyGeneratorCommand extends Command
     public function handle(): int
     {
         if (app()->runningUnitTests()) {
+            $keyBases = [];
             $numKeys = 5;
         } else {
-
+            $keyBases = multiselect('Generate Keys for which key bases?', [
+                null => 'All',
+                ...collect(config('cipher.ciphers')->map(fn($value, $key) => $key))->toArray(),
+            ]);
             $numKeys = text('How many keys per KeyBase?', '5', 5, false, 'int');
         }
 
         $keyGenerator = new KeyGenerator();
-        $keys = $keyGenerator->generate([], $numKeys);
+        $keys = $keyGenerator->generate(array_filter($keyBases), $numKeys);
 
         $storage = config('cipher.storage');
 
-        Storage::disk($storage['disk'])
-               ->put($storage['path'].DIRECTORY_SEPARATOR.$storage['filename'], $keys->toJson());
+        $this->updateKeys($keys, $storage);
 
         return CommandAlias::SUCCESS;
+    }
+
+    /**
+     * @throws \JsonException
+     */
+    private function updateKeys(Collection $keys, array $storage): void
+    {
+        $keysToUpdate = $this->getKeysToUpdate($keys, $storage);
+        if ($keysToUpdate?->count()) {
+            Storage::disk($storage['disk'])
+                   ->put($storage['path'].DIRECTORY_SEPARATOR.$storage['filename'], $keys->toJson());
+        }
+    }
+
+    /**
+     * @throws \JsonException
+     */
+    private function getKeysToUpdate(Collection $keys, array $storage): Collection
+    {
+        $existingKeyData = Storage::disk($storage['disk'])
+                                  ->get($storage['path'].DIRECTORY_SEPARATOR.$storage['filename']);
+
+        if (empty($existingKeyData)) {
+            return $keys;
+        }
+
+        $existingKeys = collect(json_decode($existingKeyData, true, 512, JSON_THROW_ON_ERROR));
+
+        $diff = array_diff_key($existingKeys->toArray(), $keys->toArray());
+
+        if (!count($diff)) {
+            throw new \RuntimeException('Keys already existing, unable to overwrite existing keys');
+        }
+
+        return $keys->merge($existingKeys);
     }
 }
--- tests/Feature/KeyGeneratorCommandTest.php
+++ tests/Feature/KeyGeneratorCommandTest.php
@@ -6,17 +6,34 @@ use Illuminate\Support\Facades\Storage;
 
 class KeyGeneratorCommandTest extends TestCase
 {
+    public array $storage;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->storage = config('cipher.storage');
+
+        Storage::fake($this->storage['disk']);
+    }
+
     /** @test */
     public function it_can_save_the_keys_as_a_file(): void
     {
-        $storage = config('cipher.storage');
-
-        Storage::fake($storage['disk']);
-
-        Storage::disk($storage['disk'])->assertMissing($storage['path'].DIRECTORY_SEPARATOR.$storage['filename']);
+        Storage::disk($this->storage['disk'])->assertMissing($this->storage['path'].DIRECTORY_SEPARATOR.$this->storage['filename']);
 
         Artisan::call('cipher:generate-keys');
 
-        Storage::disk($storage['disk'])->assertExists($storage['path'].DIRECTORY_SEPARATOR.$storage['filename']);
+        Storage::disk($this->storage['disk'])->assertExists($this->storage['path'].DIRECTORY_SEPARATOR.$this->storage['filename']);
+    }
+
+    /** @test */
+    public function it_prevents_overwriting_existing_keys(): void
+    {
+        Artisan::call('cipher:generate-keys');
+
+        $this->expectException(RuntimeException::class);
+
+        Artisan::call('cipher:generate-keys');
     }
 }
